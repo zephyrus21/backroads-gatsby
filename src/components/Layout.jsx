845f398b01abadd04e07b141bcbce62692507413
zephyrus21 <pandey.piyus@gmail.com>
2021-01-29 00:19:28 +0530

    adding images

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+import './layout.css';
+
 const Layout = (props) => {
   return (
     <>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <Layout>
-      Hello Piyush Pandey!
-      <Link to="/blog">Blog</Link>
-    </Layout>
-  );
+  return <Layout>Hello Piyush Pandey!</Layout>;
 }
