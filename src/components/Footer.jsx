89c24d2d2cef74dc4764e20af9b185840374161c
zephyrus21 <pandey.piyus@gmail.com>
2021-01-29 11:44:43 +0530

    editing fototer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,23 @@ import social from '../constants/social-links';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.links}></div>
+      <div className={styles.links}>
+        {links.map((item, index) => (
+          <Link key={index} to={item.path}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        {social.map((item, index) => (
+          <a key={index} href={item.url} target="_blank" rel="noreferrer">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; Piyush {new Date().getFullYear}
+      </div>
     </footer>
   );
 };
