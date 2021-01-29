0167c4555099a65b6c681fe156b6ea395fc9a0f5
zephyrus21 <pandey.piyus@gmail.com>
2021-01-29 15:15:17 +0530

    adding styled components

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Title from '../Title.styled';
+const About = () => {
+  return (
+    <div>
+      About
+      <Title title="About" subtitle="Us" />
+    </div>
+  );
+};
+
+export default About;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import SimpleHero from '../components/SimpleHero';
 import Banner from '../components/Banner';
+import About from '../components/Home/About';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
           </Link>
         </Banner>
       </SimpleHero>
+      <About />
     </Layout>
   );
 }
