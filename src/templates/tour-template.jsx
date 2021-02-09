d278f52a672394cbf53fcf9d6b6c46162d321052
zephyrus21 <pandey.piyus@gmail.com>
2021-02-10 00:46:13 +0530

    tours complete

--- src/templates/tour-template.jsx
+++ src/templates/tour-template.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { FaMoneyBillWave, FaMap } from 'react-icons/fa';
+
+import Layout from '../components/Layout';
+import StyledHero from '../components/StyledHero';
+import styles from '../css/template.module.css';
 
 export const query = graphql`
   query($slug: String!) {
@@ -34,7 +40,34 @@ const TourTemplate = ({ data }) => {
 
   const [mainImage, ...tourImages] = images;
 
-  return <div>{name}</div>;
+  return (
+    <Layout>
+      <StyledHero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {tourImages.map((item, index) => (
+              <Img key={index} fluid={item.fluid} className={styles.image} />
+            ))}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>Starts on : {start}</h4>
+          <h4>duration : {days}</h4>
+          <p className={styles.desc}>{description}</p>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default TourTemplate;
