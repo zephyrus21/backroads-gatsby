68165051de2122a4c59fabd4cf1f2cdb135b126f
zephyrus21 <pandey.piyus@gmail.com>
2021-02-09 21:06:38 +0530

    adding queries

--- src/templates/tour-template.jsx
+++ src/templates/tour-template.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const TourTemplate = () => {
-  return <div>Tour Template</div>;
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`;
+
+const TourTemplate = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    start,
+    description: { description },
+    images,
+  } = data.tour;
+
+  const [mainImage, ...tourImages] = images;
+
+  return <div>{name}</div>;
 };
 
 export default TourTemplate;
